e1360d153646bbd299d385c6d6a06720169dac1d
Truong Tuoc Phi <truongphi754@gmail.com>
2024-06-29 23:38:27 +0700

    up

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   upload: {
     config: {
       provider: "cloudinary",
-      jwtSecret: "nf40BYgx3giqc69NnPelPA==",
+      jwtSecret: env("JWT_SECRET", "nf40BYgx3giqc69NnPelPA=="),
       providerOptions: {
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_KEY"),
