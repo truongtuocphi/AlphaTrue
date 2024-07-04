070bd34a768ea23346db6096390701437511a881
Truong Tuoc Phi <truongphi754@gmail.com>
2024-07-04 10:30:03 +0700

    fix: src

--- frontend/src/app/blog/[id]/page.tsx
+++ frontend/src/app/blog/[id]/page.tsx
@@ -1,9 +1,5 @@
-import {
-  BlocksRenderer,
-  type BlocksContent,
-} from "@strapi/blocks-react-renderer";
-import Image from "next/image";
-import { getPostById, getPosts } from "@/config/api.config";
+import { type BlocksContent } from "@strapi/blocks-react-renderer";
+import { getPostById } from "@/config/api.config";
 import { formatDate } from "@/utils/dateUtils";
 import BlockRendererClient from "@/app/blog/_components/BlockRendererClient";
 
@@ -17,8 +13,8 @@ export default async function BlogDeltail({
   const payload = await getPostById(getIdPost);
   const dataPost = payload.data;
 
-  const content: BlocksContent = dataPost?.attributes?.content;
-  console.log(content);
+  const contentPost: BlocksContent = dataPost?.attributes?.content;
+
   return (
     dataPost && (
       <div className="max-w-4xl mx-auto px-5 pt-10 pb-12 lg:px-10 lg:pt-20 lg:pb-32">
@@ -32,43 +28,7 @@ export default async function BlogDeltail({
           </div>
         </div>
         <div className="border-t-2 py-8 border-b-2">
-          {/* <BlocksRenderer
-            content={content}
-            blocks={{
-              image: async ({ image }) => {
-                "use server";
-                console.log(image);
-                return (
-                  <Image
-                    className="my-2"
-                    src={image.url}
-                    alt={image.name}
-                    width={image.width}
-                    height={image.height}
-                  />
-                );
-              },
-            }}
-          /> */}
-          <BlockRendererClient content={content} />
-        </div>
-        <div className="w-full pt-6">
-          <div className="flex flex-col gap-14">
-            <h1 className="font-semibold">NEWS & ANNOUNCEMENTS</h1>
-            {/* {DataPosts &&
-              DataPosts.map((post, index) => (
-                <a href={`/blog/${index + 1}`} key={index}>
-                  <div className="flex items-center gap-14">
-                    <div className="text-xs font-semibold text-gray-500">
-                      {formatDate(post.createdAt)}
-                    </div>
-                    <h2 className="text-xl lg:text-2xl font-thin">
-                      {post.title}
-                    </h2>
-                  </div>
-                </a>
-              ))} */}
-          </div>
+          <BlockRendererClient content={contentPost} />
         </div>
       </div>
     )
