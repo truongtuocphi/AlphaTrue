ea79aaf8ec51ba7a222c4ab8645227b06ecc049e
Truong Tuoc Phi <truongphi754@gmail.com>
2024-07-10 13:01:26 +0700

    update

--- frontend/src/app/blog/[slug]/page.tsx
+++ frontend/src/app/blog/[slug]/page.tsx
@@ -23,13 +23,15 @@ export default async function BlogDeltail({
         <div className="max-w-4xl mx-auto px-5 pt-10 pb-12 lg:px-10 lg:pt-20 lg:pb-32">
           <div className="flex flex-col gap-3 mb-4">
             <h1 className="text-2xl lg:text-4xl">
-              {dataPost.attributes?.title}
+              {dataPost?.attributes?.title}
             </h1>
             <div className="text-sm text-gray-600 mb-12">
               <div>
-                Date Submitted: {formatDate(dataPost.attributes?.createdAt)}
+                Date Submitted: {formatDate(dataPost?.attributes?.createdAt)}
               </div>
-              <div>Edit date: {formatDate(dataPost.attributes?.updatedAt)}</div>
+              <div>
+                Edit date: {formatDate(dataPost?.attributes?.updatedAt)}
+              </div>
             </div>
           </div>
           <div className="border-t-2 py-8 border-b-2">
