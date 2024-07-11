4dacdf28c546bec95066e024dd584cf102fd86c1
Truong Tuoc Phi <truongphi754@gmail.com>
2024-07-11 10:30:07 +0700

    update

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
+import { listItemFooter } from "@/lib/data";
 import Link from "next/link";
 import IconLogoWhite from "./icons/IconLogoWhite";
 import IconArrow from "./icons/IconArrow";
-import { listItemFooter } from "@/lib/data";
 
 export default function Footer() {
   return (
