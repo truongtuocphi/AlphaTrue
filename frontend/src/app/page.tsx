4c101e900fb60489e7b8dcf7cf7d70f3c7d91cdc
Truong Tuoc Phi <truongphi754@gmail.com>
2024-07-16 10:42:14 +0700

    feat: component SectionCompanyHistory

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,9 @@
-import IconLogo from "@/components/icons/IconLogo";
+import SectionCompanyHistory from "@/components/Home/SectionCompanyHistory";
 import { getData } from "@/config/api.config";
 
 export default async function Home() {
   const payload = await getData("posts");
   const postList = payload.data;
 
-  return (
-    <div className="h-svh">
-      <div className="flex justify-center w-full">
-        <h5 className="text-2xl text-black-10 font-normal text-center">
-          The true solution for the alpha
-          <br />
-          projects in the decentralized world
-        </h5>
-      </div>
-      <div className="w-full flex justify-center"></div>
-    </div>
-  );
+  return <div className="w-full">Home</div>;
 }
---
+++ frontend/src/components/Home/SectionCompanyHistory.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Rectangle from "@/public/images/Home/Rectangle 3.png";
+
+export default function SectionCompanyHistory() {
+  return (
+    <section>
+      <div className="max-w-fit flex gap-8">
+        <div className="w-96">
+          <h4 className="text-4xl text-third-50 mb-6 font-bold">
+            History - Story
+          </h4>
+          <p className="text-base text-black-50">
+            AlphaTrue offers a multi-sectoral strategy and a one-stop solution
+            for your business in the blockchain industry.
+            <br />
+            <br />
+            We work at the crossroads of sincere, empirical, and technological
+            research. AlphaTrue primary focuses are customer experience,
+            advanced products, and clarity and efficiency in the management
+            process.
+          </p>
+        </div>
+
+        <div className="max-w-full h-80">
+          <Image src={Rectangle} alt="Rectangle" className="w-full h-full" />
+        </div>
+      </div>
+    </section>
+  );
+}
