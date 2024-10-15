9e3c43d31b9f48734fe54c7176078fe3c023d272
truongphi <truongphi754@gmail.com>
2024-10-15 14:19:38 +0700

    feat: update

--- frontend/src/components/Home/SectionAchievements.tsx
+++ frontend/src/components/Home/SectionAchievements.tsx
@@ -4,14 +4,12 @@ import TimeLine from "./TimeLine";
 export default function SectionAchievements() {
   return (
     <div className="w-full mt-12 lg:mt-52">
-      <div className="flex flex-col text-center gap-6 mb-10">
+      <div className="flex flex-col text-center gap-4 mb-10">
         <>
           <AnimationSection
             className={`animate-fade-right animate-once animate-duration-[2000ms]`}
           >
-            <h4 className="text-4xl font-bold text-second-50 mb-3">
-              Achievements
-            </h4>
+            <h4 className="text-4xl font-bold text-second-50">Achievements</h4>
           </AnimationSection>
           <AnimationSection
             className={`animate-fade-left animate-once animate-duration-[2000ms]`}
