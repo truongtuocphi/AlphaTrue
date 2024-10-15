a273c51f098ffac886d1f7c7cf0677c902da82ed
truongphi <truongphi754@gmail.com>
2024-10-15 13:48:13 +0700

    feat: update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-
-import LogoAPT from "@/public/images/Home/APT.gif";
 import { getData } from "@/config/api.config";
 
 import SectionBanner from "@/components/Home/SectionBanner";
 import SectionBlog from "@/components/Home/SectionBlog";
 import SectionCompanyHistory from "@/components/Home/SectionCompanyHistory";
 import SectionStakeholders from "@/components/Home/SectionStakeholders";
-import SectionTeam from "@/components/Home/SectionTeam";
 import SectionAchievements from "@/components/Home/SectionAchievements";
 import ContactFrom from "@/components/Home/ContactFrom";
 
