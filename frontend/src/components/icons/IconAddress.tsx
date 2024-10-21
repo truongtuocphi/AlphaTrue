72a509db3ebaaea77ca84b0fc560daa2e1de269c
truongphi <truongphi754@gmail.com>
2024-10-21 10:57:01 +0700

    feat: update

--- frontend/src/components/icons/IconAddress.tsx
+++ frontend/src/components/icons/IconAddress.tsx
@@ -22,7 +22,7 @@ const IconAddress: React.FC<IconAddressProps> = ({
         d="M10 10.4167C11.3807 10.4167 12.5 9.29746 12.5 7.91675C12.5 6.53604 11.3807 5.41675 10 5.41675C8.6193 5.41675 7.50001 6.53604 7.50001 7.91675C7.50001 9.29746 8.6193 10.4167 10 10.4167Z"
         stroke="#303433"
         stroke-width="1.5"
-        stroke-linecap="round"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
       <path
@@ -30,14 +30,14 @@ const IconAddress: React.FC<IconAddressProps> = ({
         stroke="white"
         stroke-opacity="0.2"
         stroke-width="1.5"
-        stroke-linecap="round"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M10 18.3334C11.6667 15.0001 16.6667 12.8486 16.6667 8.33342C16.6667 4.65152 13.6819 1.66675 10 1.66675C6.31811 1.66675 3.33334 4.65152 3.33334 8.33342C3.33334 12.8486 8.33334 15.0001 10 18.3334Z"
         stroke="#303433"
         stroke-width="1.5"
-        stroke-linecap="round"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
       <path
@@ -45,7 +45,7 @@ const IconAddress: React.FC<IconAddressProps> = ({
         stroke="white"
         stroke-opacity="0.2"
         stroke-width="1.5"
-        stroke-linecap="round"
+        strokeLinecap="round"
         stroke-linejoin="round"
       />
     </svg>
