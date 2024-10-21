754974788a25c25e6441ae482651b4206a37eedf
truongphi <truongphi754@gmail.com>
2024-10-21 11:06:17 +0700

    feat: update

--- frontend/src/components/icons/IconTwitterCircle.tsx
+++ frontend/src/components/icons/IconTwitterCircle.tsx
@@ -18,7 +18,7 @@ const IconTwitterCircle: React.FC<IconTwitterCircleProps> = ({
       viewBox="0 0 36 36"
       fill="none"
     >
-      <g clip-path="url(#clip0_1432_4317)">
+      <g clipPath="url(#clip0_1432_4317)">
         <path
           d="M18 36C27.9411 36 36 27.9411 36 18C36 8.05887 27.9411 0 18 0C8.05887 0 0 8.05887 0 18C0 27.9411 8.05887 36 18 36Z"
           fill="#303433"
@@ -26,7 +26,7 @@ const IconTwitterCircle: React.FC<IconTwitterCircleProps> = ({
         <path
           d="M18 36C27.9411 36 36 27.9411 36 18C36 8.05887 27.9411 0 18 0C8.05887 0 0 8.05887 0 18C0 27.9411 8.05887 36 18 36Z"
           fill="white"
-          fill-opacity="0.2"
+          fillOpacity="0.2"
         />
         <path
           d="M23.5083 10.5073H26.179L20.3444 17.1759L27.2083 26.2504H21.8339L17.6244 20.7468L12.8079 26.2504H10.1356L16.3763 19.1176L9.79167 10.5073H15.3026L19.1075 15.5378L23.5083 10.5073ZM22.571 24.6519H24.0508L14.4984 12.0219H12.9104L22.571 24.6519Z"
